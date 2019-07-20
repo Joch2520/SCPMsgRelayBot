194349b7233dfa020111f085446e898b3430d6e3
Joch <20432565+Joch2520@users.noreply.github.com>
2019-07-20 15:09:22 +0800

    kek

--- master.js
+++ master.js
@@ -17,8 +17,8 @@ fs.readdir("./events/", (err, files) => {
   files.forEach(file => {
     let eventFunction = require(`./events/${file}`);
     let eventName = file.split(".")[0];
-    // super-secret recipe to call events with all their proper arguments *after* the `bot` var.
-    client.on(eventName, (...args) => eventFunction.run(bot, ...args));
+    // super-secret recipe to call events with all their proper arguments *after* the `client` var.
+    client.on(eventName, (...args) => eventFunction.run(client, ...args));
   });
 });
 
