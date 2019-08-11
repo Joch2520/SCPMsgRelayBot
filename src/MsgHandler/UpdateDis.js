80dba5f611e1ab6e8f0d2d24591ceff719411818
Joch <20432565+Joch2520@users.noreply.github.com>
2019-08-12 00:45:40 +0800

    added sending files when updating discord msg

--- src/MsgHandler/UpdateDis.js
+++ src/MsgHandler/UpdateDis.js
@@ -3,10 +3,10 @@ exports.run = (client, editFrom, src) => {
   var targetMsg = client.channels.get(editFrom.targetChan).fetchMessage(editFrom.msg_id);
   switch (editFrom.type) {
     case "normal":
-      targetMsg.edit(editFrom.sender + ': ' + editFrom.content);
+      targetMsg.edit(editFrom.sender + ': ' + editFrom.content, {files:editFrom.files});
       break;
     case "embed":
-      targetMsg.edit(editFrom.sender + ': ', {embed:editFrom.embed} );
+      targetMsg.edit(editFrom.sender + ': ', {files:editFrom.files, embed:editFrom.embed});
       break;
     default:
       targetMsg.edit(editFrom.sender + ': ' + editFrom.content);
