9bca16d36defb367e6ae6b5590155d2cc5e632f7
Joch <20432565+Joch2520@users.noreply.github.com>
2019-08-18 01:45:22 +0800

    fixed wrong client object

--- src/MsgHandler/ToQQ.js
+++ src/MsgHandler/ToQQ.js
@@ -1,4 +1,4 @@
-exports.run = (clients, receive, src) => {
+exports.run = (client, receive, src) => {
   const path = require("path");
   const SQLite = require('better-sqlite3');
   const MsgMap = new SQLite(path.join(__dirname,'../../data/MsgMappings.sqlite'));
@@ -10,7 +10,7 @@ exports.run = (clients, receive, src) => {
   let TToQMap = MsgMap.prepare("INSERT OR REPLACE INTO FromTel (TelMsgID, QQMsgID) VALUES (@TelMsgID, @QQMsgID);");
 
   if (src.from.toLowerCase() === "dis") {
-    clients.qq('send_group_msg',receive).then(res => {
+    client('send_group_msg',receive).then(res => {
       if (src.from === "dis") {
         DToQMap.run({DisMsgID:src.id, QQMsgID:res.message_id.toString(10)});
       } else if (src.from === "tel") {
