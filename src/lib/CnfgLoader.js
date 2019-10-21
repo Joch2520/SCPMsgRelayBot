95390244b17a1f540705baeea4e23b43ab5238a7
Joch2520 <20432565+Joch2520@users.noreply.github.com>
2019-10-21 16:51:57 +0800

    fixed if env var is undefined

--- src/lib/CnfgLoader.js
+++ src/lib/CnfgLoader.js
@@ -17,10 +17,10 @@ confignames = ["CMD_PREFIX", "DIS_TOKEN", "TEL_TOKEN", "SCP_SITE"];
 for (name of confignames) { if (custom[name] !== undefined && custom[name]) {config[name] = custom[name]} };
 
 if (process.env.MRB_FORCE_ENV === undefined || process.env.MRB_FORCE_ENV.toLowerCase === "true") {
-  if (process.env.MRB_CMD_PREFIX) { config.CMD_PREFIX = process.env.MRB_CMD_PREFIX };
-  if (process.env.MRB_DIS_TOKEN) { config.DIS_TOKEN = process.env.MRB_DIS_TOKEN };
-  if (process.env.MRB_TEL_TOKEN) { config.TEL_TOKEN = process.env.MRB_TEL_TOKEN };
-  if (process.env.MRB_SCP_SITE) { config.SCP_SITE = process.env.MRB_SCP_SITE };
+  if (process.env.MRB_CMD_PREFIX!==undefined && process.env.MRB_CMD_PREFIX) { config.CMD_PREFIX = process.env.MRB_CMD_PREFIX };
+  if (process.env.MRB_DIS_TOKEN !==undefined && process.env.MRB_DIS_TOKEN ) { config.DIS_TOKEN  = process.env.MRB_DIS_TOKEN  };
+  if (process.env.MRB_TEL_TOKEN !==undefined && process.env.MRB_TEL_TOKEN ) { config.TEL_TOKEN  = process.env.MRB_TEL_TOKEN  };
+  if (process.env.MRB_SCP_SITE  !==undefined && process.env.MRB_SCP_SITE  ) { config.SCP_SITE   = process.env.MRB_SCP_SITE   };
 }
 
 
